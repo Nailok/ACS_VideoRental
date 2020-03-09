8e3cbb88164477d9b1df683b58fc308efb3d0c05
Nailok <lexitor34@gmail.com>
2020-03-09 20:03:27 +0300

    Small fixes

--- adminMenu.php
+++ adminMenu.php
@@ -1,8 +1,8 @@
 <form method="post" action="adminMenu.php">
-    <div class="home"> <a href="home.php" name="home" > Home </a>  </div>
-    <div class="products"> <a href="addProductsToDB.php" name="products" > Add products </a>  </div>
-
+    
+    <div class="home"> <a href="home.php" name="home" > Главная </a>  </div>
+    <div class="addProducts"> <a href="addProductsToDB.php" name="add_products" > Добавить фильм </a>  </div>
+    <div class="products"> <a href = "products.php" name ="products" > Список </a> </div>
 </form> 
-
 
     
