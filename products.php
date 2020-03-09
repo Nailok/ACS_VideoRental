f744c9f44a92dd18cf726347dd09d8193adb85f5
Nailok <lexitor34@gmail.com>
2020-03-09 20:03:35 +0300

    Small changes

--- products.php
+++ products.php
@@ -1,1 +1,24 @@
-<?php include('server.php') ?>
+<?php
+    session_start();
+
+    $db = mysqli_connect('localhost', 'phpmyadmin', 'Alexander34', 'account');
+mysqli_query($db, "SET NAMES UTF8");
+    $query = "SELECT * FROM products ORDER BY id ASC";
+    $product_array = mysqli_query($db, $query);
+if (!empty($product_array)) { 
+	foreach($product_array as $key=>$value){
+?>
+	<div class="product-item">
+		<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["id"]; ?>">
+		<div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
+		<div class="product-tile-footer">
+		<div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
+        <div class="product-title"><?php echo $product_array[$key]["quantity"]; ?></div>
+		<div class="product-price"><?php echo "$".$product_array[$key]["price"]; ?></div>
+		</div>
+		</form>
+	</div>
+<?php
+	}
+}
+?>
--- server.php
+++ server.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 // initializing variables
@@ -10,6 +11,8 @@ $products = "";
 // connect to the database
 $db = mysqli_connect('localhost', 'phpmyadmin', 'Alexander34', 'account');
 mysqli_query($db, "SET NAMES UTF8");
+/
+    
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
@@ -20,40 +23,46 @@ if (isset($_POST['reg_user'])) {
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
-  }
+    if (empty($username)) {
+          array_push($errors, "Username is required");
+    }
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Password is required");
+    }
+    if ($password_1 != $password_2) {
+            array_push($errors, "The two passwords do not match");
+    }
 
-  // first check the database to make sure 
+  // first check the database to make sure
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-  $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
+    $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+    $result = mysqli_query($db, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
   
-  if ($user) { // if user exists
-    if ($user['username'] === $username) {
-      array_push($errors, "Username already exists");
-    }
+    if ($user) { // if user exists
+        if ($user['username'] === $username) {
+             array_push($errors, "Username already exists");
+        }
 
-    if ($user['email'] === $email) {
-      array_push($errors, "email already exists");
+        if ($user['email'] === $email) {
+            array_push($errors, "email already exists");
+        }
     }
-  }
 
   // Finally, register user if there are no errors in the form
-  if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+    if (count($errors) == 0) {
+        $password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO users (username, email, password, isAdmin) 
+        $query = "INSERT INTO users (username, email, password, isAdmin) 
   			  VALUES('$username', '$email', '$password', 0)";
-  	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
-  }
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+        header('location: index.php');
+    }
 }
 
 
@@ -62,62 +71,63 @@ if (isset($_POST['reg_user'])) {
 
 // LOGIN USER
 if (isset($_POST['login_user'])) {
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $password = mysqli_real_escape_string($db, $_POST['password']);
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
 
-  if (empty($username)) {
-  	array_push($errors, "Username is required");
-  }
-  if (empty($password)) {
-  	array_push($errors, "Password is required");
-  }
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
 
-  if (count($errors) == 0) {
-  	$password = md5($password);
-  	$query = "SELECT isAdmin FROM users WHERE username='$username' AND password='$password' limit 1";
-  	$results = mysqli_query($db, $query);
-    $isAdmin = !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT isAdmin FROM users WHERE username='$username' AND password='$password' LIMIT 1";
+        $results = mysqli_query($db, $query);
+        // $isAdmin = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        $isAdmin_get = mysqli_fetch_array($results);
+        $isAdmin = $isAdmin_get['isAdmin'];
+        $_SESSION['isAdmin'] = $isAdmin;
 
-  	$_SESSION['isAdmin'] = $isAdmin;
-    
-    if (mysqli_num_rows($results) == 1) {
+        if (mysqli_num_rows($results) == 1) {
       
       
-  	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: index.php');
-  	}else {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        } else {
         
-  		array_push($errors, "Wrong username/password combination");
-  	}
-  }
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
 }
 
 
 //ADD FILM TO DATABASE
 
-if (isset($_POST['film_add'])){
+if (isset($_POST['film_add'])) {
     $film_name = mysqli_real_escape_string($db, $_POST['film_name']);
     $film_price = mysqli_real_escape_string($db, $_POST['film_price']);
     $film_description = mysqli_real_escape_string($db, $_POST['film_description']);
     $film_quantity = mysqli_real_escape_string($db, $_POST['film_quantity']);
     
-    if(empty($film_name)){
+    if (empty($film_name)) {
         array_push($errors, 'Введите имя фильма');
         
     }
     
-    if(empty($film_price)){
+    if (empty($film_price)) {
         array_push($errors, 'Введите цену');
         
     }
     
-    if(empty($film_description)){
+    if (empty($film_description)) {
         array_push($errors, 'Введите описание фильма');
         
     }
     
-    if(empty($film_quantity)){
+    if (empty($film_quantity)) {
         array_push($errors, 'Введите кол-во дисков на складе');
     }
     
@@ -125,11 +135,11 @@ if (isset($_POST['film_add'])){
     
     
     
-    if(count($errors) == 0){
+    if (count($errors) == 0) {
         $query = "INSERT INTO products (name, description, quantity, price, created) 
   		VALUES('$film_name', '$film_description', '$film_quantity', '$film_price', now())";
         $results = mysqli_query($db, $query);
-  	    if (mysqli_num_rows($results) == 1) {
+        if (mysqli_num_rows($results) == 1) {
               $_SESSION['film_name'] = $film_name;
               $_SESSION['success'] = "Фильм был добавлен в базу данных";
               header('location: addProductsToDB.php');
@@ -137,4 +147,8 @@ if (isset($_POST['film_add'])){
     }
 }
 
+//Retrieve products from database
+
+
+    
 ## $products = mysql_query($db, "SELECT * FROM products WHERE id='1'");
